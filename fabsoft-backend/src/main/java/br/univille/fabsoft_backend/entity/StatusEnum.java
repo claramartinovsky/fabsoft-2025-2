95fb63814335f301b7409a3ac2d979adc76d766e
Guilherme Fiedler <177897732+GuilhermeFiedler@users.noreply.github.com>
2025-09-03 23:03:57 +0000

    Pequena correção Enum

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/StatusEnum.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/StatusEnum.java
@@ -1,12 +1,7 @@
 package br.univille.fabsoft_backend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 public enum StatusEnum {
-     ATIVO,
+    ATIVO,
     INATIVO,
     PENDENTE
 }
