322945b97e672138e3b280d6ced06b77d42c7637
Clara Meister Martinovsky <141659527+claramartinovsky@users.noreply.github.com>
2025-09-03 00:45:24 +0000

    aula0209

--- fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Resenha.java
+++ fabsoft-backend/src/main/java/br/univille/fabsoft_backend/entity/Resenha.java
@@ -1,5 +1,9 @@
 package br.univille.fabsoft_backend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Resenha {
@@ -9,5 +13,30 @@ public class Resenha {
     private long usuarioId;
     private long jogoId;
     private String conteudo;
+    
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public long getUsuarioId() {
+        return usuarioId;
+    }
+    public void setUsuarioId(long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+    public long getJogoId() {
+        return jogoId;
+    }
+    public void setJogoId(long jogoId) {
+        this.jogoId = jogoId;
+    }
+    public String getConteudo() {
+        return conteudo;
+    }
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
 }
 
