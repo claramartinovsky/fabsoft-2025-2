65db3a11cdcd6bce950ce8414fa91d5ef3477427
Guilherme Fiedler <177897732+GuilhermeFiedler@users.noreply.github.com>
2025-10-16 15:46:23 +0000

    Continuação primeira tela Angular

--- projfabsoft_frontend/src/app/usuario/usuario.component.ts
+++ projfabsoft_frontend/src/app/usuario/usuario.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { Usuario } from '../model/usuario'
+import { UsuarioService } from '../service/usuario.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+
+
 
 @Component({
   selector: 'app-usuario',
-  imports: [],
+  imports: [HttpClientModule, CommonModule],
   templateUrl: './usuario.html',
-  styleUrl: './usuario.css'
+  styleUrl: './usuario.css',
+  providers: [UsuarioService]
 })
 export class UsuarioComponent {
+  listaUsuarios: Usuario[] = []
+
+  constructor(private usuarioService: UsuarioService){}
+
+  ngOnInit(){
+    console.log('Carregando usuarios...')
+    this.usuarioService.getUsuarios().subscribe( usuarios => {
+        this.listaUsuarios = usuarios
+    } )
+  }
+
 
 }
