e3fbac87e9fffd609ce9073f9a2ca99741a8875e
Guilherme Fiedler <177897732+GuilhermeFiedler@users.noreply.github.com>
2025-11-16 14:13:03 +0000

    ManyToOne: Jogo.

--- projfabsoft_frontend/src/app/form-jogo/form-jogo.ts
+++ projfabsoft_frontend/src/app/form-jogo/form-jogo.ts
@@ -6,23 +6,43 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { Jogo } from '../model/jogo';
 
+import { Plataforma } from '../model/plataforma';
+import { PlataformaService } from '../service/plataforma.service';
+import { Categoria } from '../model/categoria';
+import { CategoriaService } from '../service/categoria.service';
+
 @Component({
   selector: 'app-form-jogo',
   imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './form-jogo.html',
   styleUrl: './form-jogo.css',
-  providers: [JogoService, Router]
+  providers: [JogoService, Router, PlataformaService, CategoriaService]
 })
 export class FormJogo {
     jogo: Jogo = new Jogo();
 
+    listaPlataformas:Plataforma[] = [];
+    listaCategorias:Categoria[] = [];
+
+
     constructor(
       private jogoService:JogoService,
       private router:Router,
-      private activeRouter: ActivatedRoute
+      private activeRouter: ActivatedRoute,
+      private plataformaService:PlataformaService,
+      private categoriaService:CategoriaService
     ){
       const id = this.activeRouter.snapshot.paramMap.get('id');
         
+      this.plataformaService.getPlataformas().subscribe(listaPlataformas =>{
+        this.listaPlataformas = listaPlataformas
+      });
+
+      this.categoriaService.getCategorias().subscribe(listaCategorias =>{
+        this.listaCategorias = listaCategorias
+      })
+
+
         if (id) {
           this.jogoService.getJogoById(id).subscribe(jogo => {
             this.jogo = jogo;
@@ -35,4 +55,13 @@ export class FormJogo {
             this.router.navigate(['jogos']);
         });
     }
+
+      comparaPlataformas(obj1: Plataforma, obj2: Plataforma): boolean{
+      return obj1 && obj2 ? obj1.id === obj2.id : obj1 === obj2
+  }
+
+    comparaCategorias(obj1: Categoria, obj2: Categoria): boolean{
+      return obj1 && obj2 ? obj1.id === obj2.id : obj1 === obj2
+  }
+
 }
--- projfabsoft_frontend/src/app/model/jogo.ts
+++ projfabsoft_frontend/src/app/model/jogo.ts
@@ -1,6 +1,11 @@
+import { Categoria } from "./categoria";
+import { Plataforma } from "./plataforma";
+
 export class Jogo {
     id: number;
     nome: string;
     descricao: string;
     dataLancamento: Date;
+    plataforma: Plataforma;
+    categoria: Categoria;
 }
