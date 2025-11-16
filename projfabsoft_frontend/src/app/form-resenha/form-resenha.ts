ac1c98277f733f297406d634cfceead0003d66ca
Guilherme Fiedler <177897732+GuilhermeFiedler@users.noreply.github.com>
2025-11-16 17:01:29 +0000

    Resenhas - Completado.

--- projfabsoft_frontend/src/app/form-resenha/form-resenha.ts
+++ projfabsoft_frontend/src/app/form-resenha/form-resenha.ts
@@ -6,23 +6,41 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
+import { Usuario } from '../model/usuario';
+import { UsuarioService } from '../service/usuario.service';
+
+import { Jogo } from '../model/jogo';
+import { JogoService } from '../service/jogo.service';
+
 @Component({
   selector: 'app-form-resenha',
   imports: [HttpClientModule, CommonModule, FormsModule],
   templateUrl: './form-resenha.html',
   styleUrl: './form-resenha.css',
-  providers: [ResenhaService, Router]
+  providers: [ResenhaService, Router, JogoService, UsuarioService]
 })
 export class FormResenha {
     resenha: Resenha = new Resenha();
 
+    listaJogos:Jogo[] = [];
+    listaUsuarios:Usuario[] = [];
     constructor(
       private resenhaService:ResenhaService,
+      private jogoService:JogoService,
+      private usuarioService:UsuarioService,
       private router:Router,
       private activeRouter: ActivatedRoute
     ){
       const id = this.activeRouter.snapshot.paramMap.get('id');
         
+        this.jogoService.getJogos().subscribe(listaJogos =>{
+          this.listaJogos = listaJogos
+        })
+
+        this.usuarioService.getUsuarios().subscribe(listaUsuarios =>{
+          this.listaUsuarios = listaUsuarios
+        })
+
         if (id) {
           this.resenhaService.getResenhaById(id).subscribe(resenha => {
             this.resenha = resenha;
@@ -36,4 +54,13 @@ export class FormResenha {
             this.router.navigate(['resenhas']);
         });
     }
+
+    comparaUsuarios(obj1: Usuario, obj2: Usuario): boolean{
+      return obj1 && obj2 ? obj1.id === obj2.id : obj1 === obj2
+  }
+
+    comparaJogos(obj1: Jogo, obj2: Jogo): boolean{
+      return obj1 && obj2 ? obj1.id === obj2.id : obj1 === obj2
+  }
+
 }
--- projfabsoft_frontend/src/app/model/resenha.ts
+++ projfabsoft_frontend/src/app/model/resenha.ts
@@ -1,4 +1,9 @@
+import { Jogo } from "./jogo";
+import { Usuario } from "./usuario";
+
 export class Resenha {
     id: number;
     conteudo: string;
+    jogo: Jogo;
+    usuario: Usuario
 }
